154c8cbeae73a06ae138392ecc841d6bcf7cf23f
Bryan Corrales <bryan230198@hotmail.com>
2026-05-14 03:26:51 -0500

    Font robotto implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,16 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import MuiProvider from "@/lib/mui/MuiProvider";
 import MainContainer from "@/components/layout/MainContainer";
+import MuiProvider from "@/lib/mui/MuiProvider";
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
-});
+  display: "swap",
+  variable: "--font-roboto",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={roboto.variable}>
+      <body>
         <MuiProvider>
           <MainContainer>{children}</MainContainer>
         </MuiProvider>
